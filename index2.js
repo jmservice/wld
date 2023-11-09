0722ac7df0d5409016c83b2df4840ee94c1b1e7d
jmservice <149927088+jmservice@users.noreply.github.com>
2023-11-09 18:43:21 +0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,43 @@
+
+    // Check if user data exists in localStorage
+    const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
+    const loginForm = document.getElementById("signup-form");
+    const resetPasswordForm = document.getElementById("reset-password-form");
+
+    
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const loginUsername = document.getElementById("login-username").value;
+        const loginPassword = document.getElementById("login-password").value;
+
+        const user = storedUsers.find(u => u.username === loginUsername && u.password === loginPassword);
+        if(loginUsername =='' ||loginPassword==''){
+          document.getElementById("login-username-error").textContent = "Please fill all the fields";
+        }
+        else if (user) {
+          document.getElementById("login-username-error").style.color = "green";
+          document.getElementById("login-username-error").textContent = "Login Successful";
+             
+             window.location.href = "worldaccount.html";
+             loginForm.reset();
+
+            // You can redirect the user to another page or perform other actions here
+        } else {
+            document.getElementById("login-username-error").textContent = "Incorrect Email or Password";
+            
+        }
+    });
+
+    
+    // Custom email validation
+    document.getElementById("signup-username").addEventListener("input", function () {
+        const emailInput = document.getElementById("signup-username").value;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(emailInput)) {
+            document.getElementById("signup-username-error").textContent = "Invalid email format. Please enter a valid email address.";
+        } else {
+            document.getElementById("signup-username-error").textContent = "";
+        }
+    });
+
